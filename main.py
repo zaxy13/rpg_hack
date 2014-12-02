b48c546b946084753cfd666a7a2d4febaccde754
zaxy <zax.zm2@gmail.com>
2014-12-02 19:04:18 +0000

    Did a lot of work on the main file, build an attack function but its not working...

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
+import random
+
 ### start of Actor area ###
 
 class Actor(object):
     """ the base class for all game characters """
-    def __init__(self, name, x,y):
+    def __init__(self, name, x,y, room):
         self.name = name
         self.level = 1
         self.health = 50
-        self.attack = 10
-        self.defence = 10
+        self.attack_points = 10
+        self.defence_points = 10
+        self.room = room
         self.pos = {"x":x,"y":y}
 
-    def move(self, x, y, room):
+    def attack(self):
+        return self.attack_points
+
+    def attack_chance(self):
+        luck = random.random()
+        luck * 10
+        if luck <= 80:
+            return True
+        else:
+            return False
+
+    def defence(self):
+        return self.defence_points
+
+    def defence_chance(self):
+        luck = random.random()
+        luck * 10
+        if luck <= 80:
+            return True
+        else:
+            return False
+
+    def move(self, x, y):
         new_pos = {"x":x,"y":y}
-        max_x = room.x
-        max_y = room.y
-        objects = room.objects
+        max_x = self.room.x
+        max_y = self.room.y
+        objects = self.room.objects
 
         if new_pos["x"] <= max_x and new_pos["y"] <= max_y:
             for obj in objects:
@@ -28,14 +53,15 @@ class Actor(object):
 
 
 class Player(Actor):
-    def __init__(self, name, x,y):
-        super(Player, self).__init__(name, x,y)
+    def __init__(self, name, x, y, room):
+        super(Player, self).__init__(name, x,y, room)
         self.gold = 100
         self.exp = 0
         self.next_level = 100
-        self.mana = 50
-        self.stamina = 50
+        # self.mana = 50
+        # self.stamina = 50 to complex right now.
         self.weapon = {"attack":10, "chance":3, "name":"basic sword"}
+        # todo, make an item class, to will make objects much neater.
 
     def Levelup(self):
         if self.exp >= self.next_level:
@@ -46,12 +72,10 @@ class Player(Actor):
         else:
             print "you didn't level up"
 
-    def battle(self, opponent):
-        pass
 
 class Enemy(Actor):
-    def __init__(self, name, x,y):
-        super(Enemy, self).__init__(name, x,y)
+    def __init__(self, name, x,y, room):
+        super(Enemy, self).__init__(name, x,y ,room)
         self.gold = 10
         self.loot = {}
 
@@ -79,10 +103,47 @@ class Room(object):
             else:
                 self.objects.append(new_obj)
                 print "object added"
+        # if the list is empty, the for loop will nor run.
         self.objects.append(new_obj)
         print "first object in list"
 
 ### end of Room area ###
+### start of actions ###
+
+def action_attack(attacker, oppnet):
+    """ Basic battler """
+    hit_power = attacker.attack()
+    hit_chance = attacker.attack_chance()
+    defence_chance = oppnet.defence_chance()
+    defence_power = oppnet.defence()
+
+
+    if hit_chance == True:
+        if defence_chance == True:
+            hit_total = hit_power - defence_power
+            if hit_total <= 0:
+                hit_total = 0
+            oppnet.health - hit_total
+            if oppnet.health <= 0:
+                return "oppnet dead"
+            else:
+                return "you hit with a force of %s hit points" % hit_total
+        else:
+             hit_total = hit_power
+             if hit_total <= 0:
+                 hit_total = 0
+             oppnet.health - hit_total
+             if oppnet.health <= 0:
+                 return "oppnet dead"
+             else:
+                 return "you hit with a full force of %s hit points" % hit_total
+    else:
+        return "ya missed...."
+
+
+### end of actions ###
+
+
 def main():
     pass
 
